b52be7aca2d62074586c6de855badca3c09c8e7e
Oleg Kulichkov <kuloleg52@gmail.com>
2026-04-26 21:00:14 +0300

    Avalonia project added

---
+++ app/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace app.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Hi ^_^ !";
+    public string Greeting2 { get; } = "Hi :~) !";
+}
